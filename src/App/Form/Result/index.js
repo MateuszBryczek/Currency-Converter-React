283350888fbb8302b88c5950816c50d6fe32c674
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2022-12-12 14:48:33 +0100

    script fixed

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -4,7 +4,7 @@ export const Result = ({ result }) => (
     <p className="result">
         {!!result && (
             <>
-                {result.sourseAmount?.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+                {result.sourceAmount?.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
 
                 <strong>
                     {result.targetAmount?.toFixed(2)}&nbsp;{result.currency}
